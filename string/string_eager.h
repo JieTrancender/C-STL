63063a647e91ccfa0acab12f4eafc9f1852353b0
jie-trancender <582865471@qq.com>
2017-09-10 08:33:19 +0800

    add string

---
+++ string/string_eager.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: string_eager.h
+	> Author: JieTrancender
+	> Mail: jie-email@jie-trancender.org
+	> Created Time: Sun Sep 10 08:32:53 2017
+ ************************************************************************/
+
+#ifndef _STRING_EAGER_H
+#define _STRING_EAGER_H
+#endif
